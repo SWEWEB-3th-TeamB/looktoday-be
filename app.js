710a0bd6f332ecae397ef46ce8f48d39a3b74e35
soyun0318 <thdbs0318@naver.com>
2025-08-03 23:33:32 +0900

    룩 모아보기

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/auth'); //라우트 연결
+const looksRoutes = require('./routes/looks');
 const db = require('./models'); //db
 
 const PORT = process.env.PORT || 3000;
-
-dotenv.config();
 
 const app = express();
 app.set('port',process.env.PORT || 3000);
@@ -28,6 +29,8 @@ app.get('/', (req, res) => {
 
 //라우트 연결 (/api/auth로 들어오는 요청 처리)
 app.use('/api/auth', authRoutes)
+app.use('/api/looks', looksRoutes);
+
 
 db.sequelize.sync()
   .then(() => {
--- models/index.js
+++ models/index.js
@@ -1,12 +1,33 @@
 const Sequelize = require('sequelize');
-const sequelize = require('../config/config');  // config.js에서 만든 인스턴스
+const sequelize = require('../config/config');
 
 const db = {};
+
+const User = require('./user');
+const Post = require('./post');
+const Like = require('./like');
+const Weather = require('./weather');
+const Image = require('./image');
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.User = require('./user');
-db.User.initiate(sequelize);
+db.User = User;
+db.Post = Post;
+db.Like = Like;
+db.Weather = Weather;
+db.Image = Image;
+
+User.initiate(sequelize);
+Post.initiate(sequelize);
+Like.initiate(sequelize);
+Weather.initiate(sequelize);
+Image.initiate(sequelize);
+
+User.associate(db);
+Post.associate(db);
+Like.associate(db);
+Weather.associate(db);
+Image.associate(db);
 
 module.exports = db;
