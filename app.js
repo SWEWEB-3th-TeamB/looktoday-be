9ad8ad5b89d6c45b2377b2b1450e1f70cfae0d3a
EESEO <enrui2001@naver.com>
2025-08-09 23:54:23 +0900

    feat: Initial commit with correct gitignore

--- app.js
+++ app.js
@@ -24,3 +24,10 @@ app.get('/', (req, res) => {
 app.listen(app.get('port'),()=>{
     console.log(app.get('port'),'번 포트에서 대기 중');
 });
+
+// routes 폴더에서 weather.js 파일을 불러옵니다.
+const weatherRouter = require('./routes/weather');
+
+// app.use(...) 부분에 아래 코드를 추가하여 라우터를 연결합니다.
+// 이제 /api/weather 경로로 들어오는 모든 요청은 weatherRouter가 처리합니다.
+app.use('/api/weather', weatherRouter);
