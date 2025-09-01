cdc35325235e49e699d6318e9a7dd872f6adc06d
suyeon0421 <157108684+suyeon0421@users.noreply.github.com>
2025-09-01 19:10:29 +0900

    마이페이지 수정

--- app.js
+++ app.js
@@ -75,8 +75,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 //     console.log(app.get('port'),'번 포트에서 대기 중');
 // });
 
-// routes 폴더에서 weather.js 파일을 불러옵니다.
-const weatherRouter = require('./routes/weather');
+
 
 // app.use(...) 부분에 아래 코드를 추가하여 라우터를 연결합니다.
 // 이제 /api/weather 경로로 들어오는 모든 요청은 weatherRouter가 처리합니다.
