89bb3474bb1851539b534f66549387c6e6e10ea1
EESEO <enrui2001@naver.com>
2025-09-20 23:24:01 +0900

    날씨 시간 오류 수정

--- utils/ultraTime.js
+++ utils/ultraTime.js
@@ -1,15 +1,20 @@
 // utils/ultraTime.js
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const tz = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(tz);
+
+const ZONE = 'Asia/Seoul';
+
+// 크론/수집 시각(now)을 KST 정시로 맞춰 baseDate/baseTime 생성
 function getBaseDateTimeForUltra(now = new Date()) {
-    // now는 10분 이후에 실행되는 현재 시각(크론에서 넘어옴)
-    const y = now.getFullYear();
-    const m = String(now.getMonth() + 1).padStart(2, '0');
-    const d = String(now.getDate()).padStart(2, '0');
-    const H = String(now.getHours()).padStart(2, '0');
-    // 초단기실황은 "해당 정시"를 base_time으로 사용
-    const baseDate = `${y}${m}${d}`;
-    const baseTime = `${H}00`; // 정시
-    return { baseDate, baseTime };
-  }
-  
-  module.exports = { getBaseDateTimeForUltra };
-  
+  const base = dayjs(now).tz(ZONE).startOf('hour');
+  return {
+    baseDate: base.format('YYYYMMDD'),
+    baseTime: base.format('HH00'),
+    tz: ZONE,
+  };
+}
+
+module.exports = { getBaseDateTimeForUltra };
