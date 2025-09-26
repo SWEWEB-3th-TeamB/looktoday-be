5a3cb48437dc862a60a1a0bf263aa07c8bab8439
EESEO <enrui2001@naver.com>
2025-09-27 00:34:14 +0900

    날씨 에러 수정

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,10 @@ const { verifyToken } = require('../middlewares/authMiddleware');
 // 내 정보
 router.get('/me', verifyToken, userController.getMe);
 
-// 내 지역 날씨 (프론트는 이거 하나만 호출하면 됨)
+// 내 지역 날씨 (쿼리로 si,gungu 허용)
 router.get('/me/weather', verifyToken, userController.getMyWeather);
+
+// 내 지역 저장/수정
+router.patch('/me/region', verifyToken, userController.updateMyRegion);
 
 module.exports = router;
